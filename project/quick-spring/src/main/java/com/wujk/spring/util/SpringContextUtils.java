4f9b1b949d9a9887f0fa3404b16138c2b88fe242
kevin <kevin@careerintlinc.com>
2019-02-02 13:45:22 +0800

    modify

--- project/quick-spring/src/main/java/com/wujk/spring/util/SpringContextUtils.java
+++ project/quick-spring/src/main/java/com/wujk/spring/util/SpringContextUtils.java
@@ -8,47 +8,49 @@ import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
 @Component
-public class SpringContextUtils<T>  implements ApplicationContextAware {
-    private static ApplicationContext applicationContext;
- 
-    public void setApplicationContext(ApplicationContext arg0)
-            throws BeansException {
-        applicationContext = arg0;
-    }
- 
-    /**
-     * 获取applicationContext对象
-     * @return
-     */
-    public static ApplicationContext getApplicationContext(){
-        return applicationContext;
-    }
-     
-    /**
-     * 根据bean的id来查找对象
-     * @param id
-     * @return
-     */
-    public static Object getBeanById(String id){
-        return applicationContext.getBean(id);
-    }
-     
-    /**
-     * 根据bean的class来查找对象
-     * @param c
-     * @return
-     */
-    public static <T> T getBeanByClass(Class<T> c){
-        return applicationContext.getBean(c);
-    }
-     
-    /**
-     * 根据bean的class来查找所有的对象(包括子类)
-     * @param c
-     * @return
-     */
-    public static <T> Map<String, T> getBeansByClass(Class<T> c){
-        return applicationContext.getBeansOfType(c);
-    }
-}
+public class SpringContextUtils<T> implements ApplicationContextAware {
+	private ApplicationContext applicationContext;
 
+	public void setApplicationContext(ApplicationContext arg0) throws BeansException {
+		applicationContext = arg0;
+	}
+
+	/**
+	 * 获取applicationContext对象
+	 * 
+	 * @return
+	 */
+	public ApplicationContext getApplicationContext() {
+		return applicationContext;
+	}
+
+	/**
+	 * 根据bean的id来查找对象
+	 * 
+	 * @param id
+	 * @return
+	 */
+	public Object getBeanById(String id) {
+		return applicationContext.getBean(id);
+	}
+
+	/**
+	 * 根据bean的class来查找对象
+	 * 
+	 * @param c
+	 * @return
+	 */
+	public T getBeanByClass(Class<T> c) {
+		return applicationContext.getBean(c);
+	}
+
+	/**
+	 * 根据bean的class来查找所有的对象(包括子类)
+	 * 
+	 * @param c
+	 * @return
+	 */
+	public Map<String, T> getBeansByClass(Class<T> c) {
+		return applicationContext.getBeansOfType(c);
+	}
+}
