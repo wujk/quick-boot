5c0b7114d1d0737042388271f592015d082cb769
wujk <wjk0147369@126.com>
2019-02-08 08:12:08 +0800

    mybatis生成工具

---
+++ project/quick-mybatis/src/main/java/GeneratorSqlmap.java
@@ -0,0 +1,36 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+public class GeneratorSqlmap {
+
+	public void generator() throws Exception{
+
+		List<String> warnings = new ArrayList<String>();
+		boolean overwrite = true;
+		//鎸囧畾 閫嗗悜宸ョ▼閰嶇疆鏂囦欢
+		File configFile = new File("generatorConfig.xml"); 
+		ConfigurationParser cp = new ConfigurationParser(warnings);
+		Configuration config = cp.parseConfiguration(configFile);
+		DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+		MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config,
+				callback, warnings);
+		myBatisGenerator.generate(null);
+
+	} 
+	public static void main(String[] args) throws Exception {
+		try {
+			GeneratorSqlmap generatorSqlmap = new GeneratorSqlmap();
+			generatorSqlmap.generator();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+}
